9c7bb8b402171ee68bb297cd45bf95a06b5eb7e5
Tomas Otero <billiet.tomas@gmail.com>
2023-04-10 14:13:47 +0000

    Implemented EDocumentPermission and permission skeleton

---
+++ src/enum/permission/permission.type.ts
@@ -0,0 +1,9 @@
+import EDocumentPermission from './documentPermission.enum';
+
+const EPermission = {
+  ...EDocumentPermission,
+};
+
+type EPermission = EDocumentPermission;
+
+export default EPermission;
--- src/enum/role.enum.ts
+++ src/enum/role.enum.ts
@@ -1,3 +1,5 @@
+import EPermission from './permission/permission.type';
+
 export enum ERoleName {
   SYS_ADMIN = 'SYS_ADMIN',
   CHAMPIONSHIP_ADMIN = 'CHAMPIONSHIP_ADMIN',
@@ -5,3 +7,17 @@ export enum ERoleName {
   JURY = 'JURY',
   RACE_OFFICER = 'RACE_OFFICER',
 }
+
+export const rolePermission = {
+  [ERoleName.SYS_ADMIN]: [
+    EPermission.CreateDocument,
+    EPermission.DeleteDocument,
+  ],
+  [ERoleName.CHAMPIONSHIP_ADMIN]: [
+    EPermission.CreateDocument,
+    EPermission.DeleteDocument,
+  ],
+  [ERoleName.OFFICE]: [EPermission.CreateDocument, EPermission.DeleteDocument],
+  [ERoleName.JURY]: [],
+  [ERoleName.RACE_OFFICER]: [],
+};
