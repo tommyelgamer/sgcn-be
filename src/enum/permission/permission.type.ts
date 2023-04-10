2548912fe0ff20fea6b9421ddba3e2b9fe71bc91
Tomas Otero <billiet.tomas@gmail.com>
2023-04-10 15:16:48 +0000

    Added ResultPermissions and added SCOREKEEPER Role

--- src/enum/permission/permission.type.ts
+++ src/enum/permission/permission.type.ts
@@ -1,9 +1,11 @@
 import EDocumentPermission from './documentPermission.enum';
+import EResultPermission from './resultPermission.enum';
 
 const EPermission = {
   ...EDocumentPermission,
+  ...EResultPermission,
 };
 
-type EPermission = EDocumentPermission;
+type EPermission = EDocumentPermission | EResultPermission;
 
 export default EPermission;
--- src/enum/role.enum.ts
+++ src/enum/role.enum.ts
@@ -4,6 +4,7 @@ export enum ERoleName {
   SYS_ADMIN = 'SYS_ADMIN',
   CHAMPIONSHIP_ADMIN = 'CHAMPIONSHIP_ADMIN',
   OFFICE = 'OFFICE',
+  SCOREKEEPER = 'SCOREKEEPER',
   JURY = 'JURY',
   RACE_OFFICER = 'RACE_OFFICER',
 }
@@ -12,12 +13,36 @@ export const rolePermission = {
   [ERoleName.SYS_ADMIN]: [
     EPermission.CreateDocument,
     EPermission.DeleteDocument,
+
+    EPermission.CreateResult,
+    EPermission.DeleteResult,
+    EPermission.RetrieveHiddenResults,
+    EPermission.UpdateResultHiddenStatus,
   ],
   [ERoleName.CHAMPIONSHIP_ADMIN]: [
     EPermission.CreateDocument,
     EPermission.DeleteDocument,
+
+    EPermission.CreateResult,
+    EPermission.DeleteResult,
+    EPermission.RetrieveHiddenResults,
+    EPermission.UpdateResultHiddenStatus,
   ],
-  [ERoleName.OFFICE]: [EPermission.CreateDocument, EPermission.DeleteDocument],
+  [ERoleName.OFFICE]: [
+    EPermission.CreateDocument,
+    EPermission.DeleteDocument,
+
+    EPermission.CreateResult,
+    EPermission.DeleteResult,
+    EPermission.RetrieveHiddenResults,
+    EPermission.UpdateResultHiddenStatus,
+  ],
+  [ERoleName.SCOREKEEPER]: [
+    EPermission.CreateResult,
+    EPermission.RetrieveHiddenResults,
+    EPermission.UpdateResultHiddenStatus,
+    EPermission.DeleteResult,
+  ],
   [ERoleName.JURY]: [],
   [ERoleName.RACE_OFFICER]: [],
 };
