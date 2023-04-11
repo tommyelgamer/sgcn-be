81a5bfe41a16b9ae84ec5a3b2fd57f3b89a3a4aa
Tomas Otero <billiet.tomas@gmail.com>
2023-04-11 16:27:03 +0000

    Implemented ResultReview entity

--- src/entities/requests/resultreview.entity.ts
+++ src/entities/requests/resultreview.entity.ts
@@ -10,7 +10,7 @@ import { Championship } from '../championship.entity';
 @Entity()
 export class ResultReview {
   @PrimaryGeneratedColumn({ name: 'resultreview_id' })
-  id: number;
+  id?: number;
 
   @OneToOne(() => Championship, { cascade: true })
   @JoinColumn({ name: 'audience_championship_id' })
@@ -18,4 +18,29 @@ export class ResultReview {
 
   @Column({ name: 'audience_championship_id' })
   championshipId: number;
+
+  @Column({ name: 'resultreview_requester', type: 'jsonb' })
+  requester: {
+    category: string;
+    sailNumber: string;
+  };
+
+  @Column({ name: 'resultreview_racenumber' })
+  raceNumber: string;
+
+  @Column({ name: 'resultreview_actualresult' })
+  actualResult: string;
+
+  @Column({ name: 'resultreview_requestedresult' })
+  requestedResult: string;
+
+  @Column({ name: 'resultreview_requestText' })
+  requestText?: string;
+
+  @Column({ name: 'resultreview_status', type: 'jsonb' })
+  status: {
+    status: string;
+    comment?: string;
+    date: string;
+  }[];
 }
